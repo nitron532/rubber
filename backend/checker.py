1ffafb39f7a74965a2b6a56e3ffd259880f64caf
nitron532 <89716148+nitron532@users.noreply.github.com>
2025-07-25 22:13:58 -0700

    fixed solution count and answerarea count only being 0 or 1 (dicts can only store one of each key, so additional answerareas/solutions weren't added to the dict, so i moved counting responsiblity to another dict and incremented when building the tree), question env check

--- backend/checker.py
+++ backend/checker.py
@@ -1,10 +1,6 @@
-import json
 from TexSoup import TexSoup
-environments = ["solution", "Solution", "verbatim", "Verbatim", "answerArea", "AnswerArea"]
-commands = ["item", "Item", "vspace"]
 passedCheck = True
-answerAreaCount = 0 #needs to have a vspace in it
-solutionCount = 0
+envCount = {}
 """
 store envs / commands in a dict (to support nested characteristic)
 this way i can easily check if an environment contains something / doesn't contain something
@@ -12,12 +8,18 @@ once stored in a dict, check() itself will see if it passes, then return the sta
 base level of dict can be treated as document environment since every file lives inside document env from template.tex
 """
 def storeAsDict(s): #ignores BraceGroup inside verbatim for code
+    global envCount
     structure = {}
     for section in s.contents:
+        #store text from code{} so I can check it in check()
         if hasattr(section, 'name') and section.name != "BraceGroup":
-            structure[section.name] = storeAsDict(section)
+            envCount.setdefault(section.name, 0)
+            envCount[section.name]+=1
+            if envCount[section.name] > 1:
+                structure[f"{section.name}{envCount[section.name]}"] = storeAsDict(section)
+            else:
+                structure[f"{section.name}1"] = storeAsDict(section)
     return structure
-
 
 def displayInOrder(d, indent=0): # for test purposes
     for key, value in d.items():
@@ -27,44 +29,44 @@ def displayInOrder(d, indent=0): # for test purposes
         else:
             print(" " * (indent + 4) + str(value))
 
-def checkTree(tree):
-    global passedCheck, answerAreaCount, solutionCount
+def checkTree(tree): 
+#this will grow as i add more requirements ( next one to do is code{} and what it contains, length? whitespace? to 
+# check if its just used for question description or (bad) in place of verbatim)
+    global passedCheck
+    #might wanna split these conditions
+    if "AnswerArea" not in envCount or "Solution" not in envCount:
+        print("AnswerArea or Solution not found in tex file!")
+        passedCheck = False
+        return
+    elif envCount["AnswerArea"] != 1 or envCount["Solution"] != 1:
+        print(envCount["AnswerArea"], envCount["Solution"])
+        passedCheck = False
+        return
     for key,value in tree.items():
-        if key == "AnswerArea":
-            answerAreaCount+=1
-            if "vspace" not in value: #\\vspace should be a key to an empty dict
-                #guidelines mention another option of answerArea, will need to consider this (non empty space)
-                print("vspace not found")
-                passedCheck = False
-                break
-        elif key == "Solution":
-            solutionCount+=1
+        if key.contains("AnswerArea") and not value:
+            passedCheck = False
+            return
+        elif key.contains("Question"):
+            passedCheck = False
+            return
         if isinstance(value, dict):
             checkTree(value)
 
 
 def check(pa):
-    global answerAreaCount, solutionCount
-    answerAreaCount = 0
-    solutionCount = 0
     with open(pa) as p:
         soup = TexSoup(p.read())
         tree = storeAsDict(soup)
+        print(tree)
         if not tree:
             return False #empty dict
+        #one pass loop to iterate through tree in order of insertion (item has to be first )
         for section in tree:
-            if section != "item":
+            if section != "item1":
                 print("item not found")
                 return False
             break
-            #one pass loop to iterate through tree in order of insertion (item has to be first ), allow code env in item (For variables in question description)
         checkTree(tree)
-        if answerAreaCount != 1 or solutionCount != 1:
-            print(answerAreaCount)
-            print(solutionCount)
-            return False
-        answerAreaCount = 0
-        solutionCount = 0
         return passedCheck #could return more info since i already have specific error checks
 
 
@@ -75,10 +77,11 @@ check guideline in here:
 actual guideline checking stays inside of this file !done
 return response (yes or no) to server.py !done
 server will append checker result to response object and send to frontend !done
+front end needs to display followsFormat status
 
 guidelines:
 certain amount of specific environments (must be specific amount or fail check) !done
-if any deprecated/banned environments found (question, code outside of item) fail check
+if any deprecated/banned environments found (question, code outside of item) fail check !done
 somehow fail vertical spaces instead of using the vspace command
 
 must begin with item command
@@ -87,24 +90,4 @@ only one answer environment, red flag if a bunch of newlines are used instead of
 if code{} found, check for whitespace or long length to raise red flag
 
 """
-
-
-"""
-def displayInOrder(s, indent =0):
-    for section in s.contents:
-        tabs = ""
-        for i in range(indent):
-            tabs += " "
-        indent+=1
-        if hasattr(section, 'name') and (section.name in environments or section.name in commands): #should probably only print sections that matter, also only prints beginnings, might need to print ends
-            if section.name in environments:
-                print(f"{tabs}begin {section.name}")
-                displayInOrder(section, indent)
-                print(f"{tabs}end {section.name}")
-            elif section.name in commands:
-                print(f"{tabs}{section.name}")
-                displayInOrder(section,indent)
-                """
-
-
 
